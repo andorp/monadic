f72e38bac334186a89cd9657257b62b9c33c218e
Andor Penzes <andor.penzes@gmail.com>
2015-07-07 22:23:26 +0200

    Remove DictComp from supported value assignment for sake of Python 2.6 compatibility.

--- monadic/decorator.py
+++ monadic/decorator.py
@@ -254,7 +254,7 @@ def get_name(stmt):
 
 
 value_expressions = [ast.BoolOp, ast.BinOp, ast.UnaryOp, ast.Dict, ast.ListComp,
-                     ast.DictComp, ast.Num, ast.Str, ast.Subscript, ast.List, ast.Tuple]
+                     ast.Num, ast.Str, ast.Subscript, ast.List, ast.Tuple]
 
 
 def is_value_expr(expr):
