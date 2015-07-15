ca22f9d1e8c8d20d17fd2f45356121dcb6c428e0
Andor Penzes <andor.penzes@gmail.com>
2015-07-15 21:51:15 +0200

    Add python 2.6 compatibility

--- monadic/python_ver.py
+++ monadic/python_ver.py
@@ -1,5 +1,15 @@
 import sys
 
 
-PYTHON_VERSION = sys.version_info.major
+def get_major_version():
+    version = sys.version_info
+    if hasattr(version, 'major'):
+        return version.major
+    elif isinstance(version, tuple):
+        return version[0]
+    else:
+        raise TypeError("Excected version_info or tuple")
+
+
+PYTHON_VERSION = get_major_version()
 
