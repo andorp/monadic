4eee309f4316f972c60a678e4acc0af5651de126
Andor Penzes <andor.penzes@gmail.com>
2015-07-23 13:00:42 +0200

    None monad

---
+++ monadic/monad/none.py
@@ -0,0 +1,25 @@
+from monadic.monad_def import monad
+
+
+__all__ = ['none_monad']
+
+
+def none_functor(f):
+    def fmap(x):
+        if x is None:
+             return x
+        else:
+             return f(x)
+    return fmap
+
+
+def unit(x):
+    return x
+
+
+def join(x):
+    return x
+
+
+def none_monad():
+    return monad(none_functor, join, unit)
---
+++ test/monad/test_none.py
@@ -0,0 +1,33 @@
+from monadic.decorator import monadic, monadic_comp
+from monadic.monad_def import monad_law_one, monad_law_two, monad_law_three
+from monadic.monad.none import none_monad
+
+
+from nose.tools import eq_
+
+
+test_data = [
+    None,
+    1,
+    "String"
+]
+
+
+test_monad = none_monad()
+
+
+def test_monad_law_one():
+    for xs in test_data:
+        yield monad_law_one, test_monad, xs
+
+
+def test_monad_law_two():
+    for xs in test_data:
+        yield monad_law_two, test_monad, xs
+
+
+def test_monad_law_three():
+    for xs in test_data:
+        yield monad_law_three, test_monad, xs
+
+
